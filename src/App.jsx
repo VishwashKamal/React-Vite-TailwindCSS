e16772084c380d9a8ab2515898c40c19447e0805
Vishu <vishwashkamal2000@gmail.com>
2023-06-23 21:17:16 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='text-6xl'>Hello text is here</div>
+    </>
+  )
+}
+
+export default App
